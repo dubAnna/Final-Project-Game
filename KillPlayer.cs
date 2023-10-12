c887e82f565f0ae2087204595e25c36d45e3ea0e
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 14:39:28 +0300

    Update KillPlayer.cs

--- KillPlayer.cs
+++ KillPlayer.cs
@@ -1,3 +1,9 @@
+/*скрипт для обробки ситуації коли гравець зіштовхується з об'єктом який викликає моментальну смерть гравця
+
+private void OnTriggerEnter2D(Collider2D other) викликається, коли інший об'єкт входить у колайдер поточного об'єкта:
+-перевіряє чи має об'єкт тег "Player"
+--> якщо так -  викликає метод RespawnPlayer() з LevelManager.instance
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
