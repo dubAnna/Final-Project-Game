0497b0a7765d3f1a7f39ebba4750e78990cb72d4
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 13:36:10 +0300

    Update Switch.cs

--- Switch.cs
+++ Switch.cs
@@ -1,3 +1,13 @@
+/*Скрипт для перемикача + взаємодія гравця з ним.
+
+Основні методи:
+Start() - ініціалізує змінну theSR, яка відповідає за компонент SpriteRenderer цього об'єкта на початку гри.
+OnTriggerEnter2D(Collider2D other) - викликається коли гравець зіштовхується з перемикачем:
+В залежності від значення deactivateOnSwitch активує або деактивує змінн objectToSwitch, тоді інший об'єкт в грі.
+Змінює спрайт перемикача на downSprite.
+Встановлює hasSwitched в true, щоб запобігти наступної активації перемикача.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
