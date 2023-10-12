fbe52d6717bba436ebb54eb0bffdfe8aea66d278
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 14:25:53 +0300

    Update LevelExit.cs

--- LevelExit.cs
+++ LevelExit.cs
@@ -1,3 +1,10 @@
+/*Скрипт дозволяє завершувати рівень коли гравець досягає виходу.Коли гравець потрапляє у тригер виходу, гра викликає метод EndLevel() з LevelManager
+
+Основні методи: 
+OnTriggerEnter2D(Collider2D other) метод викликається, коли гравець зіштовхується з виходом з рівня. 
+*/
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
