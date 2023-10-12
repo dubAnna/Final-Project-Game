acfbee2460799313a6d1c14d0b4b1bf293ad4142
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 13:04:28 +0300

    Update BossTankController.cs

--- BossTankController.cs
+++ BossTankController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BossTankController : MonoBehaviour
 {
+    public enum bossStates { shooting, hurt, moving, ended };
+    public bossStates currentState;
 
-    public enum bossStates {shooting, hurt, moving};
     public Transform theBoss;
     public Animator anim;
 
     [Header("Movement")]
     public float moveSpeed;
-    public Transform leftPoint,rightPoint;
+    public Transform leftPoint, rightPoint;
     private bool moveRight;
+    public GameObject mine;
+    public Transform minePoint;
+    public float timeBetweenMines;
+    private float mineCounter;
 
     [Header("Shooting")]
     public GameObject bullet;
@@ -22,17 +27,127 @@ public class BossTankController : MonoBehaviour
 
     [Header("Hurt")]
     public float hurtTime;
-    public float hurtCounter;
+    private float hurtCounter;
+    public GameObject hitBox;
 
-    // Start is called before the first frame update
+    [Header("Health")]
+    public int health = 5;
+    public GameObject explosion, winPlatform;
+    private bool isDefeated;
+    public float shotSpeedUp, mineSpedUp; 
+
     void Start()
     {
-        
+        currentState = bossStates.shooting;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case bossStates.shooting:
+                shotCounter -= Time.deltaTime;
+                if (shotCounter <= 0)
+                {
+                    shotCounter = timeBetweenShots;
+                    var newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    newBullet.transform.localScale = theBoss.localScale;
+                }
+                break;
+
+            case bossStates.hurt:
+                if (hurtCounter > 0)
+                {
+                    hurtCounter -= Time.deltaTime;
+                    if (hurtCounter <= 0)
+                    {
+                        currentState = bossStates.moving;
+                        mineCounter = 0;
+                        if (isDefeated)
+                        {
+                            theBoss.gameObject.SetActive(false);
+
+                            Instantiate(explosion, theBoss.position, theBoss.rotation);
+
+                            winPlatform.SetActive(true);
+
+                            AudioManager.instance.StopBossMusic();
+
+                            currentState = bossStates.ended;
+                        }
+                    }
+                }
+                break;
+
+            case bossStates.moving:
+                if (moveRight)
+                {
+                    theBoss.position += new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+                    if (theBoss.position.x > rightPoint.position.x)
+                    {
+                        theBoss.localScale = new Vector3(1f, 1f, 1f);
+                        moveRight = false;
+                        EndMovement();
+                    }
+                }
+                else
+                {
+                    theBoss.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+                    if (theBoss.position.x < leftPoint.position.x)
+                    {
+                        theBoss.localScale = new Vector3(-1f, 1f, 1f);
+                        moveRight = true;
+                        EndMovement();
+                    }
+                }
+                mineCounter -= Time.deltaTime;
+                if (mineCounter <= 0)
+                {
+                    mineCounter = timeBetweenMines;
+                    Instantiate(mine, minePoint.position, minePoint.rotation);
+                }
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeHit();
+        }
+    }
+
+    public void TakeHit()
+    {
+        currentState = bossStates.hurt;
+        hurtCounter = hurtTime;
+        anim.SetTrigger("Hit");
+        AudioManager.instance.PlaySFX(0);
+
+        BossTankMine[] mines = FindObjectsOfType<BossTankMine>();
+        if (mines.Length > 0)
+        {
+            foreach (BossTankMine foundMine in mines)
+            {
+                foundMine.Explode();
+            }
+        }
+        health--;
+
+        if (health <= 0)
+        {
+            isDefeated = true;
+
+        }else 
+        {
+            timeBetweenShots /= shotSpeedUp;
+            timeBetweenMines /= mineSpedUp;
+        }
+    }
+
+    private void EndMovement()
+    {
+        currentState = bossStates.shooting;
+        shotCounter = timeBetweenShots;
+        anim.SetTrigger("StopMoving");
+        hitBox.SetActive(true);
     }
 }
