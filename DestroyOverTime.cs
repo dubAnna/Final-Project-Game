677be1580d9375d9a021ea9dfe2abe172c459b1e
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 14:48:13 +0300

    Update DestroyOverTime.cs

--- DestroyOverTime.cs
+++ DestroyOverTime.cs
@@ -1,3 +1,9 @@
+/*Скрипт для додавання до об'єкта який автоматично знищується після певного часу.
+Якщо доати цей скрипт до об'єкта, то після LifeTime секунд від початку гри цей об'єкт буде автоматично знищений
+Update - викликається кожен кадр.
+Destroy(gameObject, LifeTime) - поставити об'єкт на чергу на знищення через час, вказаний в LifeTime.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
