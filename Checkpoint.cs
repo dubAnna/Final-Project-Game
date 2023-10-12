1d6da94b352d5c83cd58ee5421b3f94322240a5f
Dub Ann <144206712+dubAnna@users.noreply.github.com>
2023-10-12 14:54:41 +0300

    Update Checkpoint.cs

--- Checkpoint.cs
+++ Checkpoint.cs
@@ -1,3 +1,21 @@
+/*Скрипт дозволяє гравцю зберігати свою позицію та встановлювати новий пункт спавну після проходження контрольної точки
+
+OnTriggerEnter2D(Collider2D other) - викликається, коли об'єкт(гравець) зіштовхується із чекпоінтом:
+-->Якщо цей об'єкт має тег "Player" ---> контрольна точка активується. 
+*контрольна точка може бути використана лише раз, коли гравець досягне її
+
+CheckPointController.instance.DeactivateCheckpoints(); - викликає метод DeactivateCheckpoints() у класі CheckPointController.
+*деактивує всі інші контрольні точки інформуючи систему що новий пункт спавну встановлений.
+
+theSR.sprite = cpOn; -змінює спрайт контрольної точки на cpOn вказуючи що новий чекпоінт активовано
+
+CheckPointController.instance.SetSpawnPoint(transform.position); - викликає метод SetSpawnPoint(Vector3 spawnPos) у класі CheckPointController. 
+*метод встановлює новий пункт спавну на позиції контрольної точки коли гравець її активує.
+
+public void ResetCheckpoint() - використовується щоб скинути контрольну точку у виключений стан(коли гравець повертається до попереднього чекпоінту)
+
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
